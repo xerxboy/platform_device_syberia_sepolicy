9ad8dc6c1a48379665ee624e8a2ee546a8661ca3
xerxboy <58204220+xerxboy@users.noreply.github.com>
2020-05-15 15:40:40 -0500

    sepolicy: To AOSiP 2/2

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -10,16 +10,16 @@ endif
 endif
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/syberia/sepolicy/common/public
+    device/syberia/aosip/common/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/syberia/sepolicy/common/private
+    device/syberia/aosip/common/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/syberia/sepolicy/common/dynamic
+    device/aosip/sepolicy/common/dynamic
 else
 BOARD_SEPOLICY_DIRS += \
-    device/syberia/sepolicy/common/dynamic \
-    device/syberia/sepolicy/common/vendor
+    device/aosip/sepolicy/common/dynamic \
+    device/aosip/sepolicy/common/vendor
 endif
