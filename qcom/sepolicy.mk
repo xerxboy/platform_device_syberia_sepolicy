e8ad7c201fa13ddaa7d862f3a41f3cc42f636ae3
xerxboy <58204220+xerxboy@users.noreply.github.com>
2020-05-15 15:39:52 -0500

    sepolicy: To AOSiP

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -10,13 +10,13 @@ endif
 endif
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/syberia/sepolicy/qcom/private
+    device/aosip/sepolicy/qcom/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/syberia/sepolicy/qcom/dynamic
+    device/aosip/sepolicy/qcom/dynamic
 else
 BOARD_SEPOLICY_DIRS += \
-    device/syberia/sepolicy/qcom/dynamic \
-    device/syberia/sepolicy/qcom/vendor
+    device/aosip/sepolicy/qcom/dynamic \
+    device/aosip/sepolicy/qcom/vendor
 endif
